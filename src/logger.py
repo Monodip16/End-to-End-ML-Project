65f7c26704ec0cd7abdf17827f9f21c0cccc5e73
Monodip Das <dasmonodip108@gmail.com>
2026-09-08 04:20:51 +0530

    Initial EDA

--- src/logger.py
+++ src/logger.py
@@ -2,6 +2,7 @@ import logging
 import os
 from datetime import datetime
 
+
 LOG_FILE = f"{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.log"
 logs_path = os.path.join(os.getcwd(),"logs", LOG_FILE)
 
@@ -14,5 +15,6 @@ logging.basicConfig(
     format = "[%(asctime)s] %(levelname)s - %(message)s]",
     datefmt = "%Y-%m-%d %H:%M:%S"
 )
+
 
 
