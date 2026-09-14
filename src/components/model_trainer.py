3db06812c4eda928158742b37ca545e29c9f8b14
Monodip Das <dasmonodip108@gmail.com>
2026-09-14 23:55:39 +0530

    Model training initiated

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -5,7 +5,7 @@ from src.logger import logging
 from src.utils import save_object
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Lasso, Ridge
 from sklearn.metrics import r2_score
 from catboost import CatBoostRegressor
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, AdaBoostRegressor
@@ -13,3 +13,40 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.neighbors import KNeighborsRegressor
 from xgboost import XGBRegressor
 from dataclasses import dataclass
+
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname((__file__),"..","..")))
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join(PROJECT_ROOT, "artifacts", "model.pkl")
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def initiate_model_trainer(self, train_array, test_array,preprocessor_path):
+        logging.info("Model training initiated")
+
+        try:
+            logging.info("Splitting training and testing input data")
+            X_train, y_train, X_test, y_test = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1]
+            )
+
+            models = {
+                "Linear Regression": LinearRegression(),
+                "Lasso Regression": Lasso(),
+                "Ridge Regression": Ridge(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Random Forest": RandomForestRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor()
+
+            }
+        except Exception as e:
+            raise CustomException(e, sys)
